494fcc4ad0edfe2d14a5317ac2edb49a82072a18
Dylan Val <dylan.val@stfc.ac.uk>
2025-12-05 14:49:52 +0000

    Reading from a file

--- react-scheduler-test/app/page.tsx
+++ react-scheduler-test/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import fs from "fs/promises";
+import path from "path";
 
-export default function Home() {
+export default async function Home() {
+  const filePath = path.join(process.cwd(), "data", "example.txt");
+  const fileContents = await fs.readFile(filePath, "utf8");
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -33,6 +38,8 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+          <h1 className="max-w-xs text-xl font-bold leading-10 tracking-tight text-black dark:text-zinc-50">File Contents:</h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">{fileContents}</p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
